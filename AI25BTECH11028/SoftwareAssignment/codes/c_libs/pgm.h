884235b7d3496071cdd65ee12b24b37b218589e6
RayidiManohar <manoharrayidi93@gmail.com>
2025-11-08 20:16:11 +0000

    AI25BTECH11028

--- AI25BTECH11028/SoftwareAssignment/codes/c_libs/pgm.h
+++
@@ -1,7 +0,0 @@
-#ifndef PGM_H
-#define PGM_H
-
-double **read(char *filename, int *rows, int *cols, int *maxVal, char *format);
-void write(char *file, double **img, int rows, int cols, int maxVal, char *format);
-
-#endif
--- AI25BTECH11028/SoftwareAssignment/codes/c_libs/svd.h
+++
@@ -1,6 +0,0 @@
-#ifndef SVD_H
-#define SVD_H
-
-void simpleSVD(double **A, int r, int c, double **U, double *S, double **V, int k);
-
-#endif
